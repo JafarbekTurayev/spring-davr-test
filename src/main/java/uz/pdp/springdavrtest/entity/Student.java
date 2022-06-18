5521c705b9f770d247f73061d403271f25231ff2
JafarbekTurayev <jafarbek1997@gmail.com>
2022-06-18 16:01:38 +0500

    jsonIgnore

--- src/main/java/uz/pdp/springdavrtest/entity/Student.java
+++ src/main/java/uz/pdp/springdavrtest/entity/Student.java
@@ -1,11 +1,14 @@
 package uz.pdp.springdavrtest.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name = "student")
@@ -18,5 +21,9 @@ public class Student {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String fullName, phone;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "student")
+    private List<Payment> paymentList;
 
 }
