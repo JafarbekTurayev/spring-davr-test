aef4bbd746750d7aa4fd64aad69052f040591250
Mamirov2003 <doniyormamirov0316@gmail.com>
2022-06-20 04:26:49 +0500

    get Groups from Day

---
+++ src/main/java/uz/pdp/springdavrtest/controller/DayController.java
@@ -0,0 +1,30 @@
+package uz.pdp.springdavrtest.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import uz.pdp.springdavrtest.dto.ApiResponse;
+import uz.pdp.springdavrtest.repository.DayRepository;
+import uz.pdp.springdavrtest.repository.TimetableRepository;
+import uz.pdp.springdavrtest.service.DayService;
+
+@RequestMapping("/day")
+@RestController
+public class DayController {
+    @Autowired
+    DayRepository dayRepository;
+
+    @Autowired
+    TimetableRepository timetableRepository;
+
+    @Autowired
+    DayService dayServise;
+
+    @GetMapping("/{id}")    //dayni idsi beriladi -> dushanba = 1 ... yakshanba = 7
+    public ApiResponse getGroupsFromDay (@PathVariable Long id){
+        return dayServise.getGroups(id);
+    }
+
+}
---
+++ src/main/java/uz/pdp/springdavrtest/service/DayService.java
@@ -0,0 +1,50 @@
+package uz.pdp.springdavrtest.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import uz.pdp.springdavrtest.dto.ApiResponse;
+import uz.pdp.springdavrtest.dto.ResGroupDTO;
+import uz.pdp.springdavrtest.entity.Day;
+import uz.pdp.springdavrtest.entity.Group;
+import uz.pdp.springdavrtest.entity.Timetable;
+import uz.pdp.springdavrtest.repository.DayRepository;
+import uz.pdp.springdavrtest.repository.TimetableRepository;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Service
+public class DayService {
+
+    @Autowired
+    TimetableRepository timetableRepository;
+    @Autowired
+    DayRepository dayRepository;
+
+    public ApiResponse getGroups(Long dayId) {
+        List<Timetable> timetableList= timetableRepository.findAll();
+        Optional<Day> optionalDay = dayRepository.findById(dayId);
+        Set<Group> groupSet = new HashSet<>();
+        if (!optionalDay.isPresent()){
+            return ApiResponse.builder().success(false).message("Bunday kun yo`q").build();
+        }
+        else {
+            Day day = optionalDay.get();
+            for (Timetable timetable : timetableList) {
+                if (timetable.getDay().equals(day)){
+                    List<Group> groups = timetable.getGroup();
+                    for (Group group : groups) {
+                        groupSet.add(group);
+                    }
+                }
+            }
+            List<ResGroupDTO> collect = groupSet.stream().map(this::mapToDTO).collect(Collectors.toList());
+            System.out.println(collect);
+        return ApiResponse.builder().success(true).message("Bu kuni shu guruhlar o`qiydi!").data(collect).build();
+        }
+    }
+    public ResGroupDTO mapToDTO(Group group) {
+        return ResGroupDTO.builder().startDate(String.valueOf(group.getStartDate())).endDate(String.valueOf(group.getEndDate())).courseName(group.getCourse().getName()).teacherName(group.getTeacher().getFullName()).groupName(group.getName()).roomName(group.getRoom().getName()).statusName(group.getStatus().getName()).build();
+    }
+
+}
